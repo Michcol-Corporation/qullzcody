210a27f71d5d8a38f0cf6410aecfe6354d23d999
Michal <126108277+Michcol-Michcolek-Mikaschi@users.noreply.github.com>
2024-01-03 19:54:09 +0100

    ssdfzsdzd

--- script.js
+++ script.js
@@ -2,7 +2,23 @@ import Quill from 'quill';
 var quill = new Quill('#editor', {
         modules: {
             // konfiguracja paska narzędzi
-            toolbar: [['code-block']]
+            toolbar: [['header']],
+            header: [[1, 2, 3, 4, 5, 6, false]],
+            font: [],
+            size: [],
+            bold: 'bold',
+            italic: 'italic',
+            underline: 'underline',
+            strike: 'strike',
+            color: [],
+            background: [],
+            script: ['sub', 'super'],
+            header: [1, 2],
+            list: ['ordered', 'bullet'],
+            indent: ['-1', '+1'],
+            direction: 'rtl',
+            align: [],
+            clean: []     
         },
         theme: 'snow' 
     });
